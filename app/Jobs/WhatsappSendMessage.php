a7c6fae31b08efd35b17220cad21314872e736c7
Isnu Nasrudin <isnunas@gmail.com>
2024-08-17 08:48:34 +0700

    af

--- app/Jobs/WhatsappSendMessage.php
+++ app/Jobs/WhatsappSendMessage.php
@@ -78,13 +78,13 @@ class WhatsappSendMessage implements ShouldQueue
         $url = config('app.url');
         $url .= Storage::url($filename);
 
-        return Log::info($url);
+        Log::info($url);
 
-        Http::baseUrl(config('app.whatsapp_api'))->post('/client/sendMessage/main', [
-            // 'chatId' => substr($this->phoneNumber->formatE164(), 1) . "@c.us",
-            'chatId' => "6282228403855@c.us",
-            "contentType" => "MessageMediaFromURL",
-            "content"=> $url
-        ]);
+        // Http::baseUrl(config('app.whatsapp_api'))->post('/client/sendMessage/main', [
+        //     // 'chatId' => substr($this->phoneNumber->formatE164(), 1) . "@c.us",
+        //     'chatId' => "6282228403855@c.us",
+        //     "contentType" => "MessageMediaFromURL",
+        //     "content"=> $url
+        // ]);
     }
 }
