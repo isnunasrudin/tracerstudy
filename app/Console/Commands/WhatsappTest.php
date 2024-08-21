888723bfccd87db0a70ae8d6c46c3ac0e3da0d69
Isnu Nasrudin <isnunas@gmail.com>
2024-08-21 15:56:08 +0700

    test

--- app/Console/Commands/WhatsappTest.php
+++ app/Console/Commands/WhatsappTest.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Jobs\WhatsappSendMessage;
 use App\Models\Student;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http as FacadesHttp;
 use Propaganistas\LaravelPhone\PhoneNumber;
 
 class WhatsappTest extends Command
@@ -28,8 +29,14 @@ class WhatsappTest extends Command
      */
     public function handle()
     {
-        Student::whereHas('entries')->whereNull('notified_at')->get()->each(function($student){
-            WhatsappSendMessage::dispatch(new PhoneNumber('6282228403855', 'ID'), $student);
-        });
+        FacadesHttp::baseUrl(config('app.whatsapp_api'))->post('/client/sendMessage/main', [
+            // 'chatId' => substr($this->phoneNumber->formatE164(), 1) . "@c.us",
+            'chatId' => "6282228403855@c.us",
+            "contentType" => "string",
+            "content"=> "Ini percobaan bosque"
+        ]);
+        // Student::whereHas('entries')->whereNull('notified_at')->get()->each(function($student){
+        //     WhatsappSendMessage::dispatch(new PhoneNumber('6282228403855', 'ID'), $student);
+        // });
     }
 }
