767f2b0b159b153edec2a9eb4e753a75f1d83a48
Isnu Nasrudin <isnunas@gmail.com>
2026-07-26 16:19:17 +0700

    fix: update WhatsappSendMessage caption text for clearer confirmation response

--- app/Jobs/WhatsappSendMessage.php
+++ app/Jobs/WhatsappSendMessage.php
@@ -80,7 +80,7 @@ class WhatsappSendMessage implements ShouldQueue
             Log::info(Http::baseUrl(config('app.whatsapp_api'))->attach('image', $hasil, 'gambar.jpg')->post('/send/image', [
                 'phone' => substr($this->phoneNumber, 1),
                 'duration' => 86400 * 7,
-                'caption' => 'Jika ini benar dengan ' . $this->student->name . ', silahkan balas "YA".'
+                'caption' => 'Jika Anda adalah ' . $this->student->name . ', silahkan balas "Baik, bukti sudah saya terima".'
             ]));
         }
     }
